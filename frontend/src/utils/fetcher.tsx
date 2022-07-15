5aaf276754075506ff8f3709d3c3fe4070478742
Zechariah <zechariahtan144@gmail.com>
2022-07-16 03:36:56 +0800

    ✨ write most of api endpoints

--- frontend/src/utils/fetcher.tsx
+++ frontend/src/utils/fetcher.tsx
@@ -2,32 +2,102 @@ import axios, { AxiosError, Method } from "axios"
 
 import { WithTimestamps } from "../models"
 import { iAnswerData } from "../models/Answer"
+import { iFormData } from "../models/Form"
 import { iQuestionData } from "../models/Question"
 import { iUserData } from "../models/User"
 
 type Routes = {
 	"/answers": {
 		POST: {
-			body: {
-				question: iQuestionData<any>[]
-				answers: Omit<iAnswerData<any>, "id" | "user_id">[]
+			body: Omit<iAnswerData<any>, "id" | "user_id">[]
+			response: {
+				message: string
+			}
+		}
+	}
+	"/forms": {
+		GET: {
+			authentication: true
+			response: WithTimestamps<iFormData>[]
+		}
+		POST: {
+			authentication: true
+			body: Omit<iFormData, "id" | "user_id">
+			response: {
+				message: string
+			}
+		}
+	}
+	"/forms/{form_id}": {
+		GET: {
+			authentication: false
+			parameters: ["form_id"]
+			response: WithTimestamps<iFormData>
+		}
+		PUT: {
+			authentication: true
+			parameters: ["form_id"]
+			body: Partial<Omit<iFormData, "id" | "user_id">>
+			response: {
+				message: string
+			}
+		}
+		DELETE: {
+			authentication: true
+			parameters: ["form_id"]
+			response: {
+				message: string
 			}
 		}
 	}
 	"/forms/{form_id}/answers": {
 		GET: {
+			authentication: true
 			parameters: ["form_id"]
-			response: iAnswerData<any>[]
+			response: WithTimestamps<iAnswerData<any>>[]
+		}
+	}
+	"/forms/{form_id}/questions": {
+		GET: {
+			authentication: false
+			parameters: ["form_id"]
+			response: WithTimestamps<iQuestionData<any>>[]
+		}
+		POST: {
+			authentication: true
+			parameters: ["form_id"]
+			body: Omit<iQuestionData<any>, "id" | "form_id">
+			response: {
+				message: string
+			}
+		}
+	}
+	"/forms/{form_id}/questions/{question_id}": {
+		GET: {
+			authentication: false
+			parameters: ["form_id", "question_id"]
+			response: WithTimestamps<iQuestionData<any>>
+		}
+		PUT: {
+			authentication: true
+			parameters: ["form_id", "question_id"]
+			body: Partial<Omit<iQuestionData<any>, "id" | "form_id" | "previous_question_id">>
+			response: {
+				message: string
+			}
+		}
+		DELETE: {
+			authentication: true
+			parameters: ["form_id", "question_id"]
+			response: {
+				message: string
+			}
 		}
 	}
 	"/register": {
 		POST: {
 			authentication: false
-			body: {
-				name: string
-				email: string
-				password: string
-			}
+			body: Partial<Omit<iUserData, "id"> & { password: string }>
 			response: {
 				message: string
 				token: string
@@ -64,7 +134,7 @@ type Routes = {
 		}
 		PUT: {
 			authentication: true
-			body: Partial<iUserData>
+			body: Partial<Omit<iUserData, "id"> & { password: string }>
 			response: {
 				message: string
 				user: WithTimestamps<iUserData>
