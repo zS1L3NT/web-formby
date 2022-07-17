482657f23d210b421892ca8c14e7ff718fe323e1
Zechariah <zechariahtan144@gmail.com>
2022-07-17 11:37:22 +0800

    ✨ paragraph question component

--- frontend/src/features/forms/components/ParagraphQuestionComponent.tsx
+++ frontend/src/features/forms/components/ParagraphQuestionComponent.tsx
@@ -1,6 +1,9 @@
-import { FC, PropsWithChildren } from "react"
+import { FC, PropsWithChildren, useState } from "react"
+
+import { Textarea } from "@chakra-ui/react"
 
 import Card from "../../../components/Card"
+import EditableText from "../../../components/EditableText"
 import { ParagraphQuestion } from "../../../models/Question"
 
 const ParagraphQuestionComponent: FC<
@@ -11,7 +14,40 @@ const ParagraphQuestionComponent: FC<
 > = props => {
 	const { question, editable } = props
 
-	return <Card></Card>
+	const [title, setTitle] = useState(question.title)
+	const [description, setDescription] = useState(question.description)
+	const [paragraph, setParagraph] = useState("")
+
+	return (
+		<Card>
+			<EditableText
+				editable={editable}
+				required={true}
+				placeholder="Add a title"
+				fontSize="2xl"
+				text={title}
+				setText={setTitle}
+				color="black"
+				noOfLines={2}
+			/>
+			<EditableText
+				editable={editable}
+				placeholder="Add a description"
+				mt={2}
+				fontSize="lg"
+				text={description ?? ""}
+				setText={setDescription}
+				noOfLines={2}
+			/>
+			<Textarea
+				mt={4}
+				value={paragraph}
+				onChange={e => setParagraph(e.target.value)}
+				placeholder="Paragraph answer"
+				disabled={editable}
+			/>
+		</Card>
+	)
 }
 
 export default ParagraphQuestionComponent
