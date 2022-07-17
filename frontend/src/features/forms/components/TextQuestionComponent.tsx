132871d17a5993fb579b65631bb58a6f2ced63b7
Zechariah <zechariahtan144@gmail.com>
2022-07-17 11:35:06 +0800

    ✨ text question component

--- frontend/src/features/forms/components/TextQuestionComponent.tsx
+++ frontend/src/features/forms/components/TextQuestionComponent.tsx
@@ -1,6 +1,9 @@
-import { FC, PropsWithChildren } from "react"
+import { FC, PropsWithChildren, useState } from "react"
+
+import { Input } from "@chakra-ui/react"
 
 import Card from "../../../components/Card"
+import EditableText from "../../../components/EditableText"
 import { TextQuestion } from "../../../models/Question"
 
 const TextQuestionComponent: FC<
@@ -11,7 +14,40 @@ const TextQuestionComponent: FC<
 > = props => {
 	const { question, editable } = props
 
-	return <Card></Card>
+	const [title, setTitle] = useState(question.title)
+	const [description, setDescription] = useState(question.description)
+	const [text, setText] = useState("")
+
+	return (
+		<Card>
+			<EditableText
+				editable={editable}
+				required={true}
+				placeholder="Add a title"
+				fontSize="2xl"
+				text={title}
+				setText={setTitle}
+				color="black"
+				noOfLines={2}
+			/>
+			<EditableText
+				editable={editable}
+				placeholder="Add a description"
+				mt={2}
+				fontSize="lg"
+				text={description ?? ""}
+				setText={setDescription}
+				noOfLines={2}
+			/>
+			<Input
+				mt={4}
+				value={text}
+				onChange={e => setText(e.target.value)}
+				placeholder="Text answer"
+				disabled={editable}
+			/>
+		</Card>
+	)
 }
 
 export default TextQuestionComponent
