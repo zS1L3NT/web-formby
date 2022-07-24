822af94f26f0515c546e1ad8e0d960b21354b6d5
Zechariah <zechariahtan144@gmail.com>
2022-07-24 22:54:55 +0800

    ✨ finish question update difference algoritm

--- frontend/src/features/forms/components/Question.tsx
+++ frontend/src/features/forms/components/Question.tsx
@@ -1,13 +1,16 @@
-import { createRef, Dispatch, PropsWithChildren, SetStateAction, useState } from "react"
+import { diff } from "deep-object-diff"
+import { createRef, PropsWithChildren, useContext, useEffect, useState } from "react"
 import { Draggable } from "react-beautiful-dnd"
 
 import { ArrowDownIcon, ArrowUpIcon, CopyIcon, DeleteIcon, DragHandleIcon } from "@chakra-ui/icons"
 import {
 	Box, IconButton, Menu, MenuButton, MenuDivider, MenuItem, MenuItemOption, MenuList,
-	MenuOptionGroup
+	MenuOptionGroup, usePrevious
 } from "@chakra-ui/react"
 
 import Card from "../../../components/Card"
+import AuthContext from "../../../contexts/AuthContext"
+import useFetcher from "../../../hooks/useFetcher"
 import {
 	iChoiceQuestion, iColorQuestion, iDateTimeQuestion, iParagraphQuestion, iQuestion,
 	iSliderQuestion, iSwitchQuestion, iTableQuestion, iTextQuestion
@@ -25,7 +28,7 @@ import TextQuestion from "./questions/TextQuestion"
 export type QuestionProps<iQ extends iQuestion> = PropsWithChildren<{
 	editable: boolean
 	dirtyQuestion: iQ
-	setDirtyQuestion: Dispatch<SetStateAction<iQ>>
+	setDirtyQuestion: (dirtyQuestion: iQ) => void
 }>
 
 const Question = (
@@ -37,15 +40,124 @@ const Question = (
 ) => {
 	const { index, editable, question } = props
 
+	const { token } = useContext(AuthContext)
+	const fetcher = useFetcher()
 	const menuRef = createRef<HTMLButtonElement>()
 
 	const [dirtyQuestion, setDirtyQuestion] = useState(question)
+	const prevDirtyQuestion = usePrevious(dirtyQuestion)
+
+	useEffect(() => {
+		if (!token || !prevDirtyQuestion) return
+
+		const dq = dirtyQuestion
+		const pdq = prevDirtyQuestion
+		const difference: Record<string, any> = {}
+
+		if (dq.title !== pdq.title) {
+			difference.title = dq.title
+		}
+
+		if (dq.description !== pdq.description) {
+			difference.description = dq.description
+		}
+
+		if (dq.photo !== pdq.photo) {
+			difference.photo = dq.photo
+		}
+
+		if (dq.required !== pdq.required) {
+			difference.required = dq.required
+		}
+
+		if (dq.type !== pdq.type) {
+			difference.type = dq.type
+
+			switch (dq.type) {
+				case "choice":
+					difference.choices = dq.choices
+					difference.choice_type = dq.choice_type
+					break
+				case "slider":
+					difference.slider_min = dq.slider_min
+					difference.slider_step = dq.slider_step
+					difference.slider_max = dq.slider_max
+					break
+				case "table":
+					difference.table_columns = dq.table_columns
+					difference.table_rows = dq.table_rows
+					difference.table_type = dq.table_type
+					break
+			}
+		} else {
+			switch (dq.type) {
+				case "choice":
+					const cdq = pdq as iChoiceQuestion
+
+					if (Object.keys(diff(dq.choices, cdq.choices)).length > 0) {
+						difference.choices = dq.choices
+					}
+
+					if (dq.choice_type !== cdq.choice_type) {
+						difference.choice_type = dq.choice_type
+					}
+					break
+				case "slider":
+					const sdq = pdq as iSliderQuestion
+
+					if (dq.slider_min !== sdq.slider_min) {
+						difference.slider_min = dq.slider_min
+					}
+
+					if (dq.slider_step !== sdq.slider_step) {
+						difference.slider_step = dq.slider_step
+					}
+
+					if (dq.slider_max !== sdq.slider_max) {
+						difference.slider_max = dq.slider_max
+					}
+					break
+				case "table":
+					const tdq = pdq as iTableQuestion
+
+					if (Object.keys(diff(dq.table_columns, tdq.table_columns)).length > 0) {
+						difference.table_columns = dq.table_columns
+					}
+
+					if (Object.keys(diff(dq.table_rows, tdq.table_rows)).length > 0) {
+						difference.table_rows = dq.table_rows
+					}
+
+					if (dq.table_type !== tdq.table_type) {
+						difference.table_type = dq.table_type
+					}
+					break
+			}
+		}
+
+		if (Object.keys(difference).length > 0) {
+			fetcher(
+				{
+					url: "/forms/{form_id}/questions/{question_id}",
+					method: "PUT",
+					parameters: {
+						form_id: dirtyQuestion.form_id,
+						question_id: dirtyQuestion.id
+					},
+					body: difference,
+					token
+				},
+				{
+					toast: false
+				}
+			).then(({ data, error }) => {})
+		}
+	}, [dirtyQuestion, token])
 
 	const componentProps = {
 		editable,
 		dirtyQuestion,
-		setDirtyQuestion,
-		question
+		setDirtyQuestion
 	}
 
 	return (
