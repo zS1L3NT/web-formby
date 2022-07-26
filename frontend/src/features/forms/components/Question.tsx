0165b6422d5135f7b54caa86dd14ae162b9741c2
Zechariah Tan <zechariahtan144@gmail.com>
2022-07-26 13:49:34 +0800

    ♻️ refactor Question.tsx into smaller components

--- frontend/src/features/forms/components/Question.tsx
+++ frontend/src/features/forms/components/Question.tsx
@@ -1,13 +1,9 @@
-import { createRef, PropsWithChildren, useContext, useEffect, useState } from "react"
+import { createRef, PropsWithChildren, useContext, useEffect } from "react"
 import { Draggable } from "react-beautiful-dnd"
+import { useImmer } from "use-immer"
 
-import { CopyIcon, DeleteIcon, DragHandleIcon } from "@chakra-ui/icons"
-import {
-	AlertDialog, AlertDialogBody, AlertDialogCloseButton, AlertDialogContent, AlertDialogFooter,
-	AlertDialogHeader, AlertDialogOverlay, Box, Button, IconButton, Image, Menu, MenuButton,
-	MenuDivider, MenuItem, MenuItemOption, MenuList, MenuOptionGroup, Spinner, useBoolean,
-	useDisclosure, usePrevious
-} from "@chakra-ui/react"
+import { DragHandleIcon } from "@chakra-ui/icons"
+import { Box, IconButton, Image, useDisclosure, usePrevious } from "@chakra-ui/react"
 
 import Card from "../../../components/Card"
 import AuthContext from "../../../contexts/AuthContext"
@@ -20,6 +16,8 @@ import {
 import getQuestionDifference from "../../../utils/getQuestionDifference"
 import EditableText from "./EditableText"
 import NewQuestionButton from "./NewQuestionButton"
+import OptionsMenu from "./OptionsMenu"
+import QuestionDeleteAlert from "./QuestionDeleteAlert"
 import ChoiceQuestion from "./questions/ChoiceQuestion"
 import ColorQuestion from "./questions/ColorQuestion"
 import DateTimeQuestion from "./questions/DateTimeQuestion"
@@ -48,17 +46,15 @@ const Question = (
 	const { setQuestions } = useContext(FormContext)
 	const fetcher = useFetcher()
 	const menuRef = createRef<HTMLButtonElement>()
-	const alertCancelRef = createRef<any>()
 
 	const { isOpen, onOpen, onClose } = useDisclosure()
-	const [isDuplicating, setIsDuplicating] = useBoolean()
-	const [question, setQuestion] = useState(parentQuestion)
-	const prevQuestion = usePrevious(question)
+	const [question, setQuestion] = useImmer(parentQuestion)
+	const __question = usePrevious(question)
 
 	useEffect(() => {
-		if (!token || !prevQuestion) return
+		if (!token || !__question) return
 
-		const difference = getQuestionDifference(prevQuestion, question)
+		const difference = getQuestionDifference(__question, question)
 		if (Object.keys(difference).length > 0) {
 			fetcher(
 				{
@@ -83,100 +79,6 @@ const Question = (
 			})
 		}
 	}, [question, token])
-
-	const handleDuplicateQuestion = async () => {
-		if (!token) return
-
-		setIsDuplicating.on()
-
-		const duplicateQuestion: Omit<iQuestion, "id" | "form_id"> = {
-			previous_question_id: question.id,
-			title: question.title,
-			description: question.description,
-			photo: question.photo,
-			required: question.required,
-			type: question.type
-		}
-
-		if (question.type === "choice") {
-			const duplicateChoiceQuestion = duplicateQuestion as Omit<
-				iChoiceQuestion,
-				"id" | "form_id"
-			>
-			duplicateChoiceQuestion.choices = question.choices
-			duplicateChoiceQuestion.choice_type = question.choice_type
-		}
-
-		if (question.type === "slider") {
-			const duplicateSliderQuestion = duplicateQuestion as Omit<
-				iSliderQuestion,
-				"id" | "form_id"
-			>
-			duplicateSliderQuestion.slider_min = question.slider_min
-			duplicateSliderQuestion.slider_step = question.slider_step
-			duplicateSliderQuestion.slider_max = question.slider_max
-		}
-
-		if (question.type === "table") {
-			const duplicateTableQuestion = duplicateQuestion as Omit<
-				iTableQuestion,
-				"id" | "form_id"
-			>
-			duplicateTableQuestion.table_columns = question.table_columns
-			duplicateTableQuestion.table_rows = question.table_rows
-			duplicateTableQuestion.table_type = question.table_type
-		}
-
-		const { data } = await fetcher({
-			url: "/forms/{form_id}/questions",
-			method: "POST",
-			body: duplicateQuestion,
-			parameters: {
-				form_id: question.form_id
-			},
-			token
-		})
-
-		if (data) {
-			setQuestions(questions => {
-				questions.splice(index + 1, 0, data.question)
-
-				if (index !== questions.length - 1) {
-					questions.at(index + 2)!.previous_question_id = data.question.id
-				}
-			})
-		}
-
-		setIsDuplicating.off()
-	}
-
-	const handleDeleteQuestion = () => {
-		if (!token) return
-
-		setQuestions(questions =>
-			questions
-				.filter((_, i) => i !== index)
-				.map(q =>
-					q.previous_question_id === parentQuestion.id
-						? { ...q, previous_question_id: parentQuestion.previous_question_id }
-						: q
-				)
-		)
-		fetcher(
-			{
-				url: "/forms/{form_id}/questions/{question_id}",
-				method: "DELETE",
-				parameters: {
-					form_id: question.form_id,
-					question_id: question.id
-				},
-				token
-			},
-			{
-				toast: false
-			}
-		)
-	}
 
 	const componentProps = {
 		editable,
@@ -206,75 +108,15 @@ const Question = (
 								onClick={() => menuRef.current?.click()}
 								{...(editable ? provided.dragHandleProps : {})}
 							/>
-							<Menu>
-								<MenuButton
-									ref={menuRef}
-									as={IconButton}
-									hidden={!editable}
-									aria-label="Options"
-									pos="absolute"
-									right={4}
-									zIndex={-1}
-									minW={6}
-								/>
-								<MenuList
-									bg="hsl(220, 26%, 18%)"
-									zIndex={10}>
-									<MenuOptionGroup
-										defaultValue={parentQuestion.type}
-										onChange={type =>
-											setQuestion({
-												...question,
-												// @ts-ignore
-												type
-											})
-										}
-										title="Question Type"
-										type="radio"
-										textAlign="start">
-										<MenuItemOption value="text">Text</MenuItemOption>
-										<MenuItemOption value="paragraph">Paragraph</MenuItemOption>
-										<MenuItemOption value="color">Color</MenuItemOption>
-										<MenuItemOption value="choice">Choice</MenuItemOption>
-										<MenuItemOption value="switch">Switch</MenuItemOption>
-										<MenuItemOption value="slider">Slider</MenuItemOption>
-										<MenuItemOption value="datetime">DateTime</MenuItemOption>
-										<MenuItemOption value="table">Table</MenuItemOption>
-									</MenuOptionGroup>
-									<MenuDivider />
-									<MenuOptionGroup
-										defaultValue={parentQuestion.required ? ["required"] : []}
-										onChange={value =>
-											setQuestion({
-												...question,
-												required: value.includes("required")
-											})
-										}
-										type="checkbox">
-										<MenuItemOption value="required">Required</MenuItemOption>
-									</MenuOptionGroup>
-									<MenuItem
-										isDisabled={isDuplicating}
-										icon={
-											isDuplicating ? (
-												<Spinner
-													w={3}
-													h={3}
-												/>
-											) : (
-												<CopyIcon />
-											)
-										}
-										onClick={handleDuplicateQuestion}>
-										Duplicate
-									</MenuItem>
-									<MenuItem
-										icon={<DeleteIcon />}
-										onClick={onOpen}>
-										Delete
-									</MenuItem>
-								</MenuList>
-							</Menu>
+
+							<OptionsMenu
+								editable={editable}
+								index={index}
+								menuRef={menuRef}
+								onDelete={onOpen}
+								question={question}
+								setQuestion={setQuestion}
+							/>
 
 							<Box mr={editable ? 8 : 0}>
 								<EditableText
@@ -362,35 +204,13 @@ const Question = (
 				)}
 			</Draggable>
 
-			<AlertDialog
-				motionPreset="slideInBottom"
-				leastDestructiveRef={alertCancelRef}
-				onClose={onClose}
+			<QuestionDeleteAlert
 				isOpen={isOpen}
-				isCentered>
-				<AlertDialogOverlay />
-
-				<AlertDialogContent>
-					<AlertDialogHeader>Delete Question?</AlertDialogHeader>
-					<AlertDialogCloseButton />
-					<AlertDialogBody>
-						Are you sure you want to delete this Question? This action is irreversable!
-					</AlertDialogBody>
-					<AlertDialogFooter>
-						<Button
-							ref={alertCancelRef}
-							onClick={onClose}>
-							Cancel
-						</Button>
-						<Button
-							colorScheme="red"
-							onClick={handleDeleteQuestion}
-							ml={3}>
-							Delete
-						</Button>
-					</AlertDialogFooter>
-				</AlertDialogContent>
-			</AlertDialog>
+				onCancel={onClose}
+				index={index}
+				question={question}
+				parentQuestion={parentQuestion}
+			/>
 		</>
 	)
 }
