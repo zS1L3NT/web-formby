84da1cf3c7b4ec2af02d253d80a4df462a91c3cb
Zechariah Tan <zechariahtan144@gmail.com>
2022-08-01 18:38:51 +0800

    ✨ add rtkq again

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client"
+import { Provider as ReduxProvider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 
 import { ChakraProvider } from "@chakra-ui/react"
@@ -6,16 +7,19 @@ import { ChakraProvider } from "@chakra-ui/react"
 import App from "./App"
 import { AuthProvider } from "./contexts/AuthContext"
 import { FormProvider } from "./contexts/FormContext"
+import store from "./store"
 import theme from "./theme"
 
 createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
-		<AuthProvider>
-			<FormProvider>
-				<ChakraProvider theme={theme}>
-					<App />
-				</ChakraProvider>
-			</FormProvider>
-		</AuthProvider>
+		<ReduxProvider store={store}>
+			<AuthProvider>
+				<FormProvider>
+					<ChakraProvider theme={theme}>
+						<App />
+					</ChakraProvider>
+				</FormProvider>
+			</AuthProvider>
+		</ReduxProvider>
 	</BrowserRouter>
 )
