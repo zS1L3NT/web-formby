eeb2977fd3c472b0c9f4c2a4e186f4891bd18eab
Zechariah Tan <zechariahtan144@gmail.com>
2022-08-02 17:38:26 +0800

    🐛 fix redux store not using api from rtkq

--- src/store.tsx
+++ src/store.tsx
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 
+import api from "./api"
+
 const store = configureStore({
-	reducer: {}
+	reducer: {
+		[api.reducerPath]: api.reducer
+	},
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(api.middleware)
 })
 
 export default store
