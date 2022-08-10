1b7278602b3eaeae9cefacb18e2c9d9ff6b10997
Zechariah Tan <zechariahtan144@gmail.com>
2022-08-10 14:00:37 +0800

    ♻️ move chart to component

---
+++ src/features/forms/responses/components/ChartValueDisplay.tsx
@@ -0,0 +1,205 @@
+import { ArcElement } from "chart.js"
+import { DateTime } from "luxon"
+import { createRef, useState } from "react"
+import { Pie } from "react-chartjs-2"
+import { ChartJSOrUndefined } from "react-chartjs-2/dist/types"
+import { useNavigate } from "react-router-dom"
+
+import {
+	Box, Button, Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader,
+	ModalOverlay, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tooltip, Tr, useColorModeValue
+} from "@chakra-ui/react"
+
+import { WithTimestamps } from "../../../../models"
+import { iResponse } from "../../../../models/Response"
+import { iUser } from "../../../../models/User"
+
+const ChartValueDisplay = ({
+	id,
+	data,
+	values,
+	getModalHeader
+}: {
+	id: string
+	data: {
+		user: WithTimestamps<iUser> | null
+		response: WithTimestamps<iResponse>
+		value: string
+	}[]
+	values: string[]
+	getModalHeader: (value: string) => string
+}) => {
+	const ref = createRef<ChartJSOrUndefined<"pie", number[], string>>()
+	const navigate = useNavigate()
+
+	const [modalVoice, setModalValue] = useState<string | null>(null)
+
+	const colors = [
+		"#4dc9f6",
+		"#f67019",
+		"#f53794",
+		"#537bc4",
+		"#acc236",
+		"#166a8f",
+		"#00a950",
+		"#58595b",
+		"#8549ba"
+	]
+
+	return (
+		<>
+			<Flex justifyContent="space-evenly">
+				<Flex
+					display={{ base: "none", md: "flex" }}
+					direction="column"
+					justifyContent="center"
+					w="20rem">
+					{values.map((value, i) => (
+						<Flex
+							key={value}
+							px={2}
+							py={1}
+							alignItems="center"
+							background="card"
+							cursor="pointer"
+							_hover={{
+								filter: useColorModeValue("brightness(0.9)", "brightness(1.2)")
+							}}
+							onMouseOver={() => {
+								const chart = ref.current
+								if (chart) {
+									const rect = chart.canvas.getBoundingClientRect()
+									const arc = chart.getDatasetMeta(0).data[
+										i
+									]! as unknown as ArcElement
+									const point = arc.getCenterPoint()
+
+									chart.canvas.dispatchEvent(
+										new MouseEvent("mousemove", {
+											clientX: rect.left + point.x,
+											clientY: rect.top + point.y - 2
+										})
+									)
+								}
+							}}
+							onMouseOut={() => {
+								const chart = ref.current
+								if (chart) {
+									chart.canvas.dispatchEvent(
+										new MouseEvent("mouseout", {
+											clientX: 0,
+											clientY: 0
+										})
+									)
+								}
+							}}
+							onClick={() => setModalValue(value)}>
+							<Box
+								h={3}
+								w={3}
+								mr={2}
+								borderWidth={2}
+								borderColor="white"
+								bg={colors[i % 9]}
+							/>
+							<Text>{value}</Text>
+							<Text ml="auto">
+								{data.filter(item => item.value === value).length}
+							</Text>
+						</Flex>
+					))}
+				</Flex>
+				<Box
+					h="16rem"
+					py={4}>
+					<Pie
+						ref={ref}
+						datasetIdKey={id}
+						options={{
+							maintainAspectRatio: false
+						}}
+						data={{
+							labels: values,
+							datasets: [
+								{
+									label: id,
+									data: values.map(
+										value => data.filter(item => item.value === value).length
+									),
+									backgroundColor: values.map((_, i) => colors[i % 9])
+								}
+							]
+						}}
+					/>
+				</Box>
+			</Flex>
+			<Modal
+				size="lg"
+				isOpen={!!modalVoice}
+				onClose={() => setModalValue(null)}
+				isCentered>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader>{getModalHeader(modalVoice!)}</ModalHeader>
+					<ModalCloseButton />
+
+					<ModalBody>
+						<TableContainer
+							maxH={80}
+							overflowY="scroll">
+							<Table>
+								<Thead>
+									<Tr>
+										<Th width="fit-content">Respondent</Th>
+										<Th isNumeric>Date</Th>
+									</Tr>
+								</Thead>
+								<Tbody>
+									{data
+										.filter(item => item.value === modalVoice)
+										.sort(
+											(a, b) =>
+												new Date(b.response.created_at).getTime() -
+												new Date(a.response.created_at).getTime()
+										)
+										.map(item => {
+											const { user, response } = item
+
+											return (
+												<Tr key={response.id}>
+													<Td
+														textDecor="underline"
+														cursor="pointer"
+														onClick={() => navigate(response.id)}>
+														<Tooltip
+															label={
+																user
+																	? `View ${user.name}'s full response`
+																	: "View full response"
+															}>
+															{user?.name ?? "Anonymous User"}
+														</Tooltip>
+													</Td>
+													<Td isNumeric>
+														{DateTime.fromISO(
+															response.created_at
+														).toFormat("dd LLL yyyy, HH:mm:ss")}
+													</Td>
+												</Tr>
+											)
+										})}
+								</Tbody>
+							</Table>
+						</TableContainer>
+					</ModalBody>
+
+					<ModalFooter>
+						<Button onClick={() => setModalValue(null)}>Close</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	)
+}
+
+export default ChartValueDisplay
--- src/features/forms/responses/components/answers/ChoiceAnswers.tsx
+++ src/features/forms/responses/components/answers/ChoiceAnswers.tsx
@@ -1,17 +1,8 @@
-import { ArcElement, Chart, registerables } from "chart.js"
-import { DateTime } from "luxon"
-import { createRef, useState } from "react"
-import { Pie } from "react-chartjs-2"
-import { ChartJSOrUndefined } from "react-chartjs-2/dist/types"
-import { useNavigate } from "react-router-dom"
-
-import {
-	Box, Button, Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader,
-	ModalOverlay, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tooltip, Tr, useColorModeValue
-} from "@chakra-ui/react"
+import { Chart, registerables } from "chart.js"
 
 import { iChoiceAnswer } from "../../../../../models/Answer"
 import { iChoiceQuestion } from "../../../../../models/Question"
+import ChartValueDisplay from "../ChartValueDisplay"
 import { AnswersProps } from "../QuestionAnswers"
 
 Chart.register(...registerables)
@@ -22,181 +13,24 @@ const ChoiceAnswers = ({
 	responses,
 	users
 }: AnswersProps<iChoiceQuestion, iChoiceAnswer>) => {
-	const ref = createRef<ChartJSOrUndefined<"pie", number[], string>>()
-	const navigate = useNavigate()
-
-	const [modalChoice, setModalChoice] = useState<string | null>(null)
-
-	const colors = [
-		"#4dc9f6",
-		"#f67019",
-		"#f53794",
-		"#537bc4",
-		"#acc236",
-		"#166a8f",
-		"#00a950",
-		"#58595b",
-		"#8549ba"
-	]
-
 	return (
-		<>
-			<Flex justifyContent="space-evenly">
-				<Flex
-					display={{ base: "none", md: "flex" }}
-					direction="column"
-					justifyContent="center"
-					w="20rem">
-					{question.choices.map((choice, i) => (
-						<Flex
-							key={choice}
-							px={2}
-							py={1}
-							alignItems="center"
-							background="card"
-							cursor="pointer"
-							_hover={{
-								filter: useColorModeValue("brightness(0.9)", "brightness(1.2)")
-							}}
-							onMouseOver={() => {
-								const chart = ref.current
-								if (chart) {
-									const rect = chart.canvas.getBoundingClientRect()
-									const arc = chart.getDatasetMeta(0).data[
-										i
-									]! as unknown as ArcElement
-									const point = arc.getCenterPoint()
+		<ChartValueDisplay
+			id={question.id}
+			data={answers
+				.map(answer => {
+					const response = responses.find(response => response.id === answer.response_id)!
+					const user = users.find(user => user.id === response.user_id)
 
-									chart.canvas.dispatchEvent(
-										new MouseEvent("mousemove", {
-											clientX: rect.left + point.x,
-											clientY: rect.top + point.y - 2
-										})
-									)
-								}
-							}}
-							onMouseOut={() => {
-								const chart = ref.current
-								if (chart) {
-									chart.canvas.dispatchEvent(
-										new MouseEvent("mouseout", {
-											clientX: 0,
-											clientY: 0
-										})
-									)
-								}
-							}}
-							onClick={() => setModalChoice(choice)}>
-							<Box
-								h={3}
-								w={3}
-								mr={2}
-								borderWidth={2}
-								borderColor="white"
-								bg={colors[i % 9]}
-							/>
-							<Text>{choice}</Text>
-							<Text ml="auto">
-								{answers.filter(answer => answer.choices.includes(choice)).length}
-							</Text>
-						</Flex>
-					))}
-				</Flex>
-				<Box
-					h="16rem"
-					py={4}>
-					<Pie
-						ref={ref}
-						datasetIdKey={question.id}
-						options={{
-							maintainAspectRatio: false
-						}}
-						data={{
-							labels: question.choices,
-							datasets: [
-								{
-									label: question.title,
-									data: question.choices.map(
-										choice =>
-											answers.filter(answer =>
-												answer.choices.includes(choice)
-											).length
-									),
-									backgroundColor: question.choices.map((_, i) => colors[i % 9])
-								}
-							]
-						}}
-					/>
-				</Box>
-			</Flex>
-			<Modal
-				isOpen={!!modalChoice}
-				onClose={() => setModalChoice(null)}
-				isCentered>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader>Responses with "{modalChoice}" selected</ModalHeader>
-					<ModalCloseButton />
-
-					<ModalBody>
-						<TableContainer
-							maxH={80}
-							overflowY="scroll">
-							<Table>
-								<Thead>
-									<Tr>
-										<Th width="fit-content">Respondent</Th>
-										<Th isNumeric>Date</Th>
-									</Tr>
-								</Thead>
-								<Tbody>
-									{answers
-										.filter(answer => answer.choices.includes(modalChoice!))
-										.sort(
-											(a, b) =>
-												new Date(b.created_at).getTime() -
-												new Date(a.created_at).getTime()
-										)
-										.map(answer => {
-											const response = responses.find(
-												r => r.id === answer.response_id
-											)!
-											const user = users.find(u => u.id === response.user_id)
-
-											return (
-												<Tr key={answer.id}>
-													<Td
-														textDecor="underline"
-														cursor="pointer"
-														onClick={() => navigate(response.id)}>
-														<Tooltip
-															label={
-																user
-																	? `View ${user.name}'s full response`
-																	: "View full response"
-															}>
-															{user?.name ?? "Anonymous User"}
-														</Tooltip>
-													</Td>
-													<Td isNumeric>
-														{DateTime.fromISO(
-															answer.created_at
-														).toFormat("dd LLL yyyy, HH:mm:ss")}
-													</Td>
-												</Tr>
-											)
-										})}
-								</Tbody>
-							</Table>
-						</TableContainer>
-					</ModalBody>
-
-					<ModalFooter>
-						<Button onClick={() => setModalChoice(null)}>Close</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
-		</>
+					return answer.choices.map(choice => ({
+						value: choice,
+						user: user ?? null,
+						response
+					}))
+				})
+				.flat()}
+			values={[...question.choices].sort()}
+			getModalHeader={value => `Response with "${value}" selected`}
+		/>
 	)
 }
 
