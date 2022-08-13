2f928a94af4626469610eaeda5c680bb8ca8c54d
Zechariah <zechariahtan144@gmail.com>
2022-08-14 00:50:32 +0800

    🚑 all questions have default values

--- database/migrations/[2]_create_questions_table.php
+++ database/migrations/[2]_create_questions_table.php
@@ -23,14 +23,14 @@ return new class extends Migration
 			$table->boolean('required')->default(false);
 			$table->enum('type', ["text", "paragraph", "color", "choice", "switch", "slider", "datetime", "table"]);
 
-			$table->json('choices')->nullable();
-			$table->enum('choice_type', ['radio', 'checkbox', 'dropdown'])->nullable();
-			$table->integer('slider_min')->nullable();
-			$table->integer('slider_max')->nullable();
-			$table->integer('slider_step')->nullable();
-			$table->json('table_columns')->nullable();
-			$table->json('table_rows')->nullable();
-			$table->enum('table_type', ['radio', 'checkbox'])->nullable();
+			$table->json('choices')->default('["Choice 1"]');
+			$table->enum('choice_type', ['radio', 'checkbox', 'dropdown'])->default('radio');
+			$table->integer('slider_min')->default(0);
+			$table->integer('slider_max')->default(100);
+			$table->integer('slider_step')->default(10);
+			$table->json('table_columns')->default('["Column 1"]');
+			$table->json('table_rows')->default('["Row 1"]');
+			$table->enum('table_type', ['radio', 'checkbox'])->default('radio');
 
 			$table->foreign('form_id')->references('id')->on('forms')->cascadeOnDelete();
 
