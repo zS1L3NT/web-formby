ace1cc82ab8e256de190795a1b5e2414467d7dd3
Zechariah <zechariahtan144@gmail.com>
2022-08-13 21:48:32 +0800

    🐛 fix unauthorized toast not redirecting

--- src/components/ErrorHandler.tsx
+++ src/components/ErrorHandler.tsx
@@ -1,34 +1,33 @@
 import { useContext, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 import { useToast } from "@chakra-ui/react"
 
 import AuthContext from "../contexts/AuthContext"
 import useAppSelector from "../hooks/useAppSelector"
-import UnauthorizedToast from "./UnauthorizedToast"
 
 const ErrorHandler = () => {
-	const error = useAppSelector(state => state.error)
 	const { setToken } = useContext(AuthContext)
+	const error = useAppSelector(state => state.error)
+	const navigate = useNavigate()
 	const toast = useToast()
 
 	useEffect(() => {
 		if (!error) return
 
 		if (error.type === "Unauthorized") {
-			if (!toast.isActive("unauthorized")) {
-				toast({ render: props => <UnauthorizedToast {...props} /> })
-			}
 			setToken(null)
-		} else {
-			if (!toast.isActive(error.type + error.message)) {
-				toast({
-					id: error.type + error.message,
-					title: error.type,
-					description: error.message,
-					status: "error",
-					isClosable: true
-				})
-			}
+			navigate("/login")
+		}
+
+		if (!toast.isActive(error.type + error.message)) {
+			toast({
+				id: error.type + error.message,
+				title: error.type,
+				description: error.message,
+				status: "error",
+				isClosable: true
+			})
 		}
 	}, [error])
 
